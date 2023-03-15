a8c88ca2335734f7992342e8887ce95c07ff38c5
Francesco Checchia <frenkeltonx@gmail.com>
2023-03-15 17:05:38 +0100

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rubrica
+
+Scrivi un programma Java che simuli una rubrica. L'utente deve essere in grado di aggiungere, cercare, visualizzare e cancellare contatti dalla rubrica.
--- src/Rubrica.java
+++ src/Rubrica.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Rubrica {
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ *  Scrivi un programma Java che simuli una rubrica. L'utente deve essere in grado di aggiungere, cercare, visualizzare e cancellare contatti dalla rubrica.
+ *
+ */
+public class Rubrica {
+
+    public static void main(String[] args) {
+        ArrayList<Contatto> rubrica = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+    }
+}
+
+class Contatto {
+    private String nome;
+    private String cognome;
+    private String telefono;
+
+    public Contatto(String nome, String cognome, String telefono) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.telefono = telefono;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String toString() {
+        return nome + " " + cognome + ": " + telefono;
+    }
+
 }
