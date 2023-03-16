3fded6f112739e3dc6de9e56c4c99710fc44ade7
Francesco Checchia <frenkeltonx@gmail.com>
2023-03-16 12:38:48 +0100

    Inizio programma

--- src/Rubrica.java
+++ src/Rubrica.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 /**
  *
@@ -8,12 +9,62 @@ import java.util.Scanner;
 public class Rubrica {
 
     public static void main(String[] args) {
-        ArrayList<Contatto> rubrica = new ArrayList<>();
+        List<Contatto> rubrica = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+
+        boolean running = true;
+
+        System.out.println("" +
+                "         _      _                  _               _            _           _             _                    _              _      _                  _          _          _                 _      \n" +
+                "        /\\ \\   /\\_\\               / /\\            /\\ \\         /\\ \\       /\\ \\           / /\\                 /\\ \\           /\\ \\   /\\_\\               /\\ \\       /\\ \\       /\\ \\             /\\ \\     \n" +
+                "       /  \\ \\ / / /         _    / /  \\          /  \\ \\        \\ \\ \\     /  \\ \\         / /  \\               /  \\ \\         /  \\ \\ / / /         _    /  \\ \\     /  \\ \\     /  \\ \\           /  \\ \\    \n" +
+                "      / /\\ \\ \\\\ \\ \\__      /\\_\\ / / /\\ \\        / /\\ \\ \\       /\\ \\_\\   / /\\ \\ \\       / / /\\ \\             / /\\ \\_\\       / /\\ \\ \\\\ \\ \\__      /\\_\\ / /\\ \\ \\   / /\\ \\ \\   / /\\ \\ \\         / /\\ \\ \\   \n" +
+                "     / / /\\ \\_\\\\ \\___\\    / / // / /\\ \\ \\      / / /\\ \\_\\     / /\\/_/  / / /\\ \\ \\     / / /\\ \\ \\           / / /\\/_/      / / /\\ \\_\\\\ \\___\\    / / // / /\\ \\_\\ / / /\\ \\_\\ / / /\\ \\ \\       / / /\\ \\ \\  \n" +
+                "    / / /_/ / / \\__  /   / / // / /\\ \\_\\ \\    / / /_/ / /    / / /    / / /  \\ \\_\\   / / /  \\ \\ \\         / / / ______   / / /_/ / / \\__  /   / / // / /_/ / // / /_/ / // / /  \\ \\_\\     / / /  \\ \\_\\ \n" +
+                "   / / /__\\/ /  / / /   / / // / /\\ \\ \\___\\  / / /__\\/ /    / / /    / / /    \\/_/  / / /___/ /\\ \\       / / / /\\_____\\ / / /__\\/ /  / / /   / / // / /__\\/ // / /__\\/ // / /   / / /    / / /    \\/_/ \n" +
+                "  / / /_____/  / / /   / / // / /  \\ \\ \\__/ / / /_____/    / / /    / / /          / / /_____/ /\\ \\     / / /  \\/____ // / /_____/  / / /   / / // / /_____// / /_____// / /   / / /    / / /          \n" +
+                " / / /\\ \\ \\   / / /___/ / // / /____\\_\\ \\  / / /\\ \\ \\  ___/ / /__  / / /________  / /_________/\\ \\ \\   / / /_____/ / // / /\\ \\ \\   / / /___/ / // / /      / / /      / / /___/ / /    / / /________   \n" +
+                "/ / /  \\ \\ \\ / / /____\\/ // / /__________\\/ / /  \\ \\ \\/\\__\\/_/___\\/ / /_________\\/ / /_       __\\ \\_\\ / / /______\\/ // / /  \\ \\ \\ / / /____\\/ // / /      / / /      / / /____\\/ /    / / /_________\\  \n" +
+                "\\/_/    \\_\\/ \\/_________/ \\/_____________/\\/_/    \\_\\/\\/_________/\\/____________/\\_\\___\\     /____/_/ \\/___________/ \\/_/    \\_\\/ \\/_________/ \\/_/       \\/_/       \\/_________/     \\/____________/  \n" +
+                "                                                                                                                                                                                                      "
+        );
+
+        while (running) {
+            System.out.println("Seleziona un'operazione:");
+            System.out.println("1. Aggiungi contatto");
+            System.out.println("2. Cerca contatto");
+            System.out.println("3. Visualizza tutti i contatti");
+            System.out.println("4. Cancella contatto");
+            System.out.println("0. Esci");
+
+            int scelta = input.nextInt();
+
+            switch (scelta) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Arrivederci!");
+                    break;
+                default:
+                    System.out.println("Errore input non valido!");
+            }
+        }
 
     }
 }
 
+/**
+ * Contatto della rubrica
+ *
+ *
+ */
 class Contatto {
     private String nome;
     private String cognome;
@@ -25,6 +76,10 @@ class Contatto {
         this.telefono = telefono;
     }
 
+    /**
+     * Metodo che restituisce il nome memorizzato all'interno del contatto
+     * @return String nome
+     */
     public String getNome() {
         return nome;
     }
@@ -35,6 +90,10 @@ class Contatto {
 
     public String getTelefono() {
         return telefono;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String toString() {
